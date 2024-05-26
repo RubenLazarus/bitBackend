b9ef1ebf36aec75b9d06970b358b9e7373f14fb6
RubenLazarus <rubenlazarus19@gmail.com>
2024-05-26 23:23:58 +0530

    otp wala

--- src/otp/otp.module.ts
+++ src/otp/otp.module.ts
@@ -3,9 +3,11 @@ import { OtpService } from './otp.service';
 import { Services } from 'src/utils/constants';
 import { MongooseModule } from '@nestjs/mongoose';
 import { entities } from 'src/utils/entities';
+import { CommanModule } from 'src/comman/comman.module';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
-  imports:[MongooseModule.forFeature(entities)],
+  imports:[HttpModule,CommanModule,MongooseModule.forFeature(entities)],
   providers: [{
     provide:Services.OTP,
     useClass:OtpService
--- src/otp/otp.service.ts
+++ src/otp/otp.service.ts
@@ -4,11 +4,13 @@ import { Model } from 'mongoose';
 import { otp, otpDetails } from 'src/entities/otp.entity';
 import { verifyOtp } from 'src/utils/types';
 import { UserDetail } from "otpless-node-js-auth-sdk"
+import { firstValueFrom, map } from 'rxjs';
+import { HttpService } from '@nestjs/axios';
 
 @Injectable()
 export class OtpService {
     @InjectModel(otp.name) private otp: Model<otpDetails>
-    constructor() { }
+    constructor(private httpservice: HttpService) { }
     async generateOTP() {
         const otp = Math.floor(1000 + Math.random() * 9000).toString();
         return otp;
@@ -73,19 +75,39 @@ export class OtpService {
         const today = new Date();
         const resetPasswordExpires = new Date(today);
         resetPasswordExpires.setMinutes(today.getMinutes() + 15);
-        const otp = await UserDetail.sendOTP(`91${data?.mobileNo}`,"","SMS","","",60,6,process.env.OTP_CLIENTID,process.env.OTP_CLIENTSECRET,"")
-        if(otp?.orderId){
+        console.log(process.env.OTP_CLIENTID,process.env.OTP_CLIENTSECRET)
+        let body ={
+            "phoneNumber": `91${data?.mobileNo}`,
+            "channel": "SMS",
+            "otpLength": 6,
+            "expiry": 60
+          }
+       
+        let otp:any= await firstValueFrom(
+            this.httpservice
+              .post(
+                `https://auth.otpless.app/auth/otp/v1/send`,
+                body,{
+                    headers: {
+                        "Content-Type":"application/json",
+                        "clientId":"BI1L7SZCVKXRZ4KAZCYE7096VDZA5KGA",
+                        "clientSecret":"ez7zv0yyllnz3u2r2zvdt09304uv1yzr"
+                    }
+                }
+              ));
+        // const otp = await UserDetail.sendOTP(`91${data?.mobileNo}`,"rubenlazarus19@gmail.com","SMS","","",60,6,process.env.OTP_CLIENTID,process.env.OTP_CLIENTSECRET)
+        if(otp.data?.orderId){
             let otpBody ={
                 mobileNo:data?.mobileNo,
                 resetPasswordExpires,
-                otp:otp?.orderId,
+                otp:otp.data?.orderId,
                 creatdAt: new Date(),  
             }
             await this.otp.create(otpBody);
         }
        
   
-        return otp
+        return otp.data
     }
     async OTPLessVerify(body){
         try {
@@ -93,11 +115,34 @@ export class OtpService {
                 .find({ mobileNo: body?.mobileNo.toLowerCase() })
                 .sort({ creatdAt: -1 });
                 // verifyOTP(email, phoneNumber, orderId, otp, clientId, clientSecret)
-        const verifiedOTP= await UserDetail.verifyOTP("",`91${body?.mobileNo}`, verify[0].otp, body?.otp, process.env.OTP_CLIENTID, process.env.OTP_CLIENTSECRET);
-        if(verifiedOTP?.verifiedOTP){
+        // const verifiedOTP= await UserDetail.verifyOTP("",`91${body?.mobileNo}`, verify[0].otp, body?.otp, process.env.OTP_CLIENTID, process.env.OTP_CLIENTSECRET);
+     let obj ={
+        orderId:verify[0].otp,
+        otp:body?.otp,
+        phoneNumber:`91${body?.mobileNo}`
+
+     }
+        let verifiedOTP:any= await firstValueFrom(
+            this.httpservice
+              .post(
+                `https://auth.otpless.app/auth/otp/v1/verify`,
+                obj,{
+                    headers: {
+                        "Content-Type":"application/json",
+                        "clientId":"BI1L7SZCVKXRZ4KAZCYE7096VDZA5KGA",
+                        "clientSecret":"ez7zv0yyllnz3u2r2zvdt09304uv1yzr"
+                    }
+                }
+              ));
+        if(verifiedOTP?.data?.isOTPVerified){
             await this.otp.deleteMany({ mobileNo: body?.mobileNo });
         }
-
+        if(!verifiedOTP.data?.success){
+            return {
+                success :false,
+                message:"Unable to Verify OTP Please Try again letar"
+            }
+        }
 
 
      
